ef5eeda4241138296bccec6434a8cbe0d3687808
Mark Rubin <markrubin@atventurepartners.tech>
2023-12-05 17:01:03 -0700

    adds models and schemas, updates database and docker files

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from sqlalchemy import Column, Integer, String
+from db.database import Base
+
+class Brewery(Base):
+    __tablename__ = "breweries"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    address = Column(String)
+    city = Column(String)
+    state_province = Column(String)
+    postal_code = Column(String)
+    latitude = Column(String, nullable=True)
+    longitude = Column(String, nullable=True)
+    phone = Column(String, nullable=True)
+    website_url = Column(String, nullable=True)
+
---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class BreweryBase(BaseModel):
+    id: int
+    name: str
+    address: str
+    city: str
+    state_province: str
+    postal_code: str
+    latitude: Optional[str] = None
+    longitude: Optional[str] = None
+    phone: Optional[str] = None
+    website_url: Optional[str] = None
+    
+class Brewery(BreweryBase):
+    id: int
