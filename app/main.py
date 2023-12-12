67ed0bce8a9fed5bea47f931347d92a720593266
Mark Rubin <markrubin@atventurepartners.tech>
2023-12-12 13:44:34 -0700

    fixes dockerignore file and has working routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from app.routes.brewery import router
 
 app = FastAPI()
+
+app.include_router(router)
 
 load_dotenv()
--- app/routes/brewery.py
+++ app/routes/brewery.py
@@ -3,6 +3,10 @@ from app.services import brewery_api
 
 router = APIRouter()
 
+@router.get("/test")
+async def test_route():
+    return {"message": "Test route working"}
+
 # Return all breweries
 @router.get('/breweries')
 async def get_breweries():
