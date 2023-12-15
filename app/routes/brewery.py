d75a07aeee82eb06c446c02d40a9b302c1111d9c
Mark Rubin <markrubin@atventurepartners.tech>
2023-12-14 20:26:14 -0700

    adds brew update schema

--- app/routes/brewery.py
+++ app/routes/brewery.py
@@ -9,6 +9,7 @@ from db.database import SessionLocal
 
 router = APIRouter()
 
+
 # Return all breweries
 @router.get('/breweries')
 async def get_breweries():
@@ -25,9 +26,9 @@ async def store_breweries():
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
-# Create a Brewery (POST request) 
+   
 
+# Create a Brewery (POST request) 
 @router.post('/create_brewery', response_model=BrewerySchema, status_code=status.HTTP_201_CREATED)
 async def create_brewery(brewery: BrewerySchema):
     '''
@@ -51,7 +52,12 @@ async def create_brewery(brewery: BrewerySchema):
         db.rollback()
         db.close()
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
+# Update Brewery
+@router.
+
+# Delete Brewery
 @router.delete('/delete_brewery/{brewery_id}', response_model=None, status_code=status.HTTP_200_OK)
 async def delete_brewery(brewery_id: str):
     db = SessionLocal()
--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 import uuid
 
+
 class BreweryBase(BaseModel):
     name: str
     brewery_type: str
@@ -17,7 +18,21 @@ class BreweryBase(BaseModel):
     
     class Config:
         orm_mode=True
-    
+
+
 class BrewerySchema(BreweryBase):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias="_id")
-    
+
+
+class BreweryUpdateSchema(BaseModel):
+    name: Optional[str] = None
+    brewery_type: Optional[str] = None
+    address: Optional[str] = None
+    city: Optional[str] = None
+    state_province: Optional[str] = None
+    postal_code: Optional[str] = None
+    country: Optional[str] = None
+    latitude: Optional[str] = None
+    longitude: Optional[str] = None
+    phone: Optional[str] = None
+    website_url: Optional[str] = None
