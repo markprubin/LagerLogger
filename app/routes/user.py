d5b4e87f75f8849a4ebad3a3ca42c771396fa3ad
Mark Rubin <mark.p.rubin@gmail.com>
2023-12-22 11:38:06 -0700

    adds get a user route

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,15 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from db.database import SessionLocal
 from app.schemas import UserCreate, User, UserBase, UserInDB
-from app.models import User
+from app.models import User as DBUser
 
 router = APIRouter()
 
 
 # Create a User
-@router.post("/users", response_model=User)
+@router.post("/users", response_model=User, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate):
     db = SessionLocal()
 
@@ -24,3 +24,16 @@ async def create_user(user: UserCreate):
         db.rollback()
         db.close()
         raise HTTPException(status_code=500, detail=str(e))
+
+# # Update User
+# @router.put('/users/{user_id', response_model=UserUpdate)
+
+
+# Get a User
+@router.get('/users/{user_id}', response_model=User)
+async def get_user(user_id: int):
+    db = SessionLocal()
+    user = db.query(DBUser).filter(DBUser.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return User(**user.__dict__)
