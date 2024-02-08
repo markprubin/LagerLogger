a6dc32beebdee160493b5ed9df9c907f8d8af93f
Mark Rubin <mark.p.rubin@gmail.com>
2024-02-07 18:03:36 -0700

    updates map_script

--- Map/map_script.py
+++
@@ -1,5 +0,0 @@
-import folium
-
-m = folium.Map(location=(33.448376, -112.074036))
-
-m.save("brew_map.html")
---
+++ map/map_script.py
@@ -0,0 +1,50 @@
+import folium
+
+from db.database import SessionLocal, Base, engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from app.api.brewery.models import Brewery
+
+
+def get_data(session: Session):
+    query = select(Brewery)
+    result = session.execute(query)
+    return result.scalars().all()
+
+
+def create_map(data):
+    m = folium.Map(location=(33.448376, -112.074036))
+
+    for brewery in data:
+        try:
+            lat = float(brewery.latitude) if brewery.latitude else None
+            lon = float(brewery.longitude) if brewery.longitude else None
+        except ValueError:
+            continue
+
+        # Add market if lat and lon are valid
+        if lat is not None and lon is not None:
+            folium.Marker(
+                tooltip=f"{brewery.name} - {brewery.brewery_type}",
+                location=[lat, lon],
+                popup=(
+                    f"{brewery.name}<br>"
+                    f"Type: {brewery.brewery_type}<br>"
+                    f"Address: {brewery.address}, {brewery.city}, {brewery.state_province}<br>"
+                    f"Phone: {brewery.phone}<br>"
+                    f"Website: <a href='{brewery.website_url}' target='_blank'>{brewery.website_url}</a>"
+                ),
+            ).add_to(m)
+
+    m.save("brew_map.html")
+
+
+def main():
+    with SessionLocal() as session:
+        data = get_data(session)
+        create_map(data)
+
+
+if __name__ == "__main__":
+    main()
